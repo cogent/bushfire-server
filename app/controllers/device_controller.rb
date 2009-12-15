a41c70f87dfeaa4fda1b73b91cbff66a17ff2dea
Michael Rowe <mrowe@mojain.com>
2009-12-15 20:04:44 +1100

    Controller creates a device.

--- app/controllers/device_controller.rb
+++ app/controllers/device_controller.rb
@@ -1,6 +1,7 @@
 class DeviceController < ActionController::Base
 
   def register_locations
+    Device.create(params[:device])
     render :nothing => true
   end
 
--- spec/controllers/device_controller_spec.rb
+++ spec/controllers/device_controller_spec.rb
@@ -2,9 +2,24 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe DeviceController do
 
+  SAMPLE_DEVICE_ID = "device_id"
+  SAMPLE_PUSH_TOKEN = "65223c62 60fa4e32 76649dd1 60e8d571 73946ec6 1516f854 0ec50d27 c89897f9"
+
   it "have an empty response" do
     post :register_locations
+    response.should be_success
     response.body.strip.should be_empty
+  end
+
+  describe "a new device" do
+
+    it "should create a device" do
+      Device.should_receive(:create).with("device_id" => SAMPLE_DEVICE_ID, "push_token" => SAMPLE_PUSH_TOKEN)
+
+      post :register_locations, :device => { "device_id" => SAMPLE_DEVICE_ID, "push_token" => SAMPLE_PUSH_TOKEN }
+      response.should be_success
+    end
+
   end
 
 end
